5d006badb0b30f1911df0d8df8ca6a194a3b94f1
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-04-30 17:43:01 +0300

    Rewrite MOV_RV and MOV_RR tests in a generic way

--- tests/cpu_instr_data_test.cc
+++ tests/cpu_instr_data_test.cc
@@ -1,141 +1,68 @@
+#include <absl/strings/str_format.h>
 #include <gtest/gtest.h>
 #include <random>
 
 #include "cpu.h"
 #include "fake_mem.h"
 
-#define TEST_MEM_SIZE 100
+#define TEST_RNG_SEED         12345
+#define TEST_NUM_RANDOM_CASES 50
+#define TEST_MEM_SIZE         100
 
-using MOV_VR_Param = std::tuple<vm_addr_t, uint8_t, uint32_t>;
-using MOV_RR_Param = std::tuple<vm_addr_t, uint8_t, uint32_t>;
+struct DataInstrParam {
+    vm_addr_t mem_base;
+    size_t cpu_exec_steps;
+    std::vector<uint8_t> instr_bytes;
 
-class MOV_VR_Test : public testing::TestWithParam<MOV_VR_Param> {
-  protected:
-    MOV_VR_Test() {
-        auto [mem_base, reg_code, val] = GetParam();
-        mem = new FakeMem(mem_base, mem_base + TEST_MEM_SIZE);
-        cpu = cpu_new(&mem->mem_if);
-        cpu->reg_pc = mem_base;
+    uint32_t expected_value;
+    std::optional<std::function<void(cpu_ctx_t *)>> f_prepare;
+    std::function<uint32_t(cpu_ctx_t *)> f_get_actual_value;
 
-        cpu_decode_reg(cpu, reg_code, &p_reg);
-        *p_reg = 0;
-        encode();
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const DataInstrParam &param) {
+        os << absl::StrFormat("{ mem_base = 0x%08X, cpu_exec_steps = %u, "
+                              "instr_bytes = <...>, expected_value = 0x%08X }",
+                              param.mem_base, param.cpu_exec_steps,
+                              param.expected_value);
+        return os;
     }
-    ~MOV_VR_Test() {
+};
+
+class DataInstrTest : public testing::TestWithParam<DataInstrParam> {
+  protected:
+    DataInstrTest() {
+        auto param = GetParam();
+
+        mem = new FakeMem(param.mem_base, param.mem_base + TEST_MEM_SIZE);
+        mem->write(param.mem_base, param.instr_bytes.data(),
+                   param.instr_bytes.size());
+
+        cpu = cpu_new(&mem->mem_if);
+        cpu->reg_pc = param.mem_base;
+    }
+    ~DataInstrTest() {
         delete cpu;
         delete mem;
     }
 
-    void encode() {
-        auto [mem_base, reg_code, val] = GetParam();
-        uint8_t instr[6] = {CPU_OP_MOV_VR, reg_code};
-        memcpy(&instr[2], &val, 4);
-        mem->write(mem_base, instr, sizeof(instr));
+    void exec_instr() {
+        auto param = GetParam();
+        for (size_t step_idx = 0; step_idx < param.cpu_exec_steps; step_idx++) {
+            cpu_step(cpu);
+            ASSERT_NE(cpu->state, CPU_HANDLE_INT);
+        }
+        ASSERT_NE(cpu->state, CPU_HANDLE_INT);
     }
 
     FakeMem *mem;
     cpu_ctx_t *cpu;
-    static constexpr int cpu_steps = 4;
-
-    uint32_t *p_reg;
 };
 
-class MOV_RR_Test : public testing::TestWithParam<MOV_RR_Param> {
-  protected:
-    MOV_RR_Test() {
-        auto [mem_base, reg_codes, val] = GetParam();
-        mem = new FakeMem(mem_base, mem_base + TEST_MEM_SIZE);
-        cpu = cpu_new(&mem->mem_if);
-        cpu->reg_pc = mem_base;
-
-        cpu_decode_reg(cpu, (reg_codes >> 4) & 0x0F, &p_reg_src);
-        cpu_decode_reg(cpu, (reg_codes >> 0) & 0x0F, &p_reg_dst);
-
-        // dst must be written first in case dst and src are the same.
-        *p_reg_dst = 0;
-        *p_reg_src = val;
-
-        encode();
-    }
-    ~MOV_RR_Test() {
-        delete cpu;
-        delete mem;
-    }
-
-    void encode() {
-        auto [mem_base, reg_codes, val] = GetParam();
-        uint8_t instr[2] = {CPU_OP_MOV_RR, reg_codes};
-        mem->write(mem_base, instr, 2);
-    }
-
-    FakeMem *mem;
-    cpu_ctx_t *cpu;
-    static constexpr int cpu_steps = 3;
-
-    uint32_t *p_reg_src;
-    uint32_t *p_reg_dst;
-};
-
-static std::map<const uint32_t *, uint32_t>
-remember_reg_values(cpu_ctx_t *ctx) {
-    std::map<const uint32_t *, uint32_t> reg_vals;
-    reg_vals[&ctx->reg_sp] = ctx->reg_sp;
-    for (size_t idx = 0; idx < CPU_NUM_GP_REGS; idx++) {
-        reg_vals[&ctx->gp_regs[idx]] = ctx->gp_regs[idx];
-    }
-    return reg_vals;
-}
-
-static size_t
-count_changed_regs(const std::map<const uint32_t *, uint32_t> &reg_vals) {
-    size_t num_changed = 0;
-    for (auto [reg_ptr, was_val] : reg_vals) {
-        uint32_t now_val = *reg_ptr;
-        if (now_val != was_val) { num_changed++; }
-    }
-    return num_changed;
-}
-
-TEST_P(MOV_VR_Test, WritesToReg) {
-    auto [mem_base, reg_code, val] = GetParam();
-    for (int step_idx = 0; step_idx < cpu_steps; step_idx++) {
-        cpu_step(cpu);
-        ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    }
-    ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    EXPECT_EQ(*p_reg, val);
-}
-
-TEST_P(MOV_VR_Test, AffectsOnlyOneReg) {
-    auto reg_vals = remember_reg_values(cpu);
-    for (int step_idx = 0; step_idx < cpu_steps; step_idx++) {
-        cpu_step(cpu);
-        ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    }
-    ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-}
-
-TEST_P(MOV_RR_Test, WritesToReg) {
-    auto [mem_base, reg_codes, val] = GetParam();
-
-    for (int step_idx = 0; step_idx < cpu_steps; step_idx++) {
-        cpu_step(cpu);
-        ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    }
-    ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-
-    EXPECT_EQ(*p_reg_src, val);
-    EXPECT_EQ(*p_reg_dst, val);
-}
-
-TEST_P(MOV_RR_Test, AffectsOnlyOneReg) {
-    auto reg_vals = remember_reg_values(cpu);
-    for (int step_idx = 0; step_idx < cpu_steps; step_idx++) {
-        cpu_step(cpu);
-        ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    }
-    ASSERT_NE(cpu->state, CPU_HANDLE_INT);
-    EXPECT_EQ(count_changed_regs(reg_vals), p_reg_src == p_reg_dst ? 0 : 1);
+TEST_P(DataInstrTest, WritesValue) {
+    auto param = GetParam();
+    if (param.f_prepare) { param.f_prepare.value()(cpu); }
+    exec_instr();
+    EXPECT_EQ(param.f_get_actual_value(cpu), param.expected_value);
 }
 
 static vm_addr_t get_random_addr(std::mt19937 &rng) {
@@ -164,25 +91,62 @@ static uint32_t get_random_imm32(std::mt19937 &rng) {
     return val_dist(rng);
 }
 
-INSTANTIATE_TEST_SUITE_P(RandomCases, MOV_VR_Test, testing::ValuesIn([&] {
-                             std::vector<MOV_VR_Param> v;
-                             std::mt19937 rng(12345);
-                             for (int i = 0; i < 50; i++) {
-                                 vm_addr_t rnd_base = get_random_addr(rng);
+INSTANTIATE_TEST_SUITE_P(Random_MOV_VR, DataInstrTest, testing::ValuesIn([&] {
+                             std::vector<DataInstrParam> v;
+                             std::mt19937 rng(TEST_RNG_SEED);
+                             for (int i = 0; i < TEST_NUM_RANDOM_CASES; i++) {
+                                 vm_addr_t mem_base = get_random_addr(rng);
                                  uint8_t reg_code = get_random_reg_code(rng);
                                  uint32_t val = get_random_imm32(rng);
-                                 v.emplace_back(rnd_base, reg_code, val);
+
+                                 DataInstrParam param;
+                                 param.mem_base = mem_base;
+                                 param.cpu_exec_steps = 4;
+
+                                 param.instr_bytes.push_back(CPU_OP_MOV_VR);
+                                 param.instr_bytes.push_back(reg_code);
+                                 param.instr_bytes.resize(6);
+                                 memcpy(param.instr_bytes.data() + 2, &val, 4);
+
+                                 param.expected_value = val;
+                                 param.f_get_actual_value =
+                                     [reg_code](cpu_ctx_t *cpu) {
+                                         uint32_t *p_reg;
+                                         cpu_decode_reg(cpu, reg_code, &p_reg);
+                                         return *p_reg;
+                                     };
+                                 v.push_back(param);
                              }
                              return v;
                          }()));
-INSTANTIATE_TEST_SUITE_P(RandomCases, MOV_RR_Test, testing::ValuesIn([&] {
-                             std::vector<MOV_RR_Param> v;
-                             std::mt19937 rng(12345);
-                             for (int i = 0; i < 50; i++) {
-                                 vm_addr_t rnd_base = get_random_addr(rng);
-                                 uint8_t reg_codes = get_random_reg_codes(rng);
-                                 uint32_t val = get_random_imm32(rng);
-                                 v.emplace_back(rnd_base, reg_codes, val);
-                             }
-                             return v;
-                         }()));
+INSTANTIATE_TEST_SUITE_P(
+    Random_MOV_RR, DataInstrTest, testing::ValuesIn([&] {
+        std::vector<DataInstrParam> v;
+        std::mt19937 rng(TEST_RNG_SEED);
+        for (int i = 0; i < TEST_NUM_RANDOM_CASES; i++) {
+            vm_addr_t mem_base = get_random_addr(rng);
+            uint8_t reg_codes = get_random_reg_codes(rng);
+            uint32_t val = get_random_imm32(rng);
+
+            DataInstrParam param;
+            param.mem_base = mem_base;
+            param.cpu_exec_steps = 3;
+
+            param.instr_bytes.push_back(CPU_OP_MOV_RR);
+            param.instr_bytes.push_back(reg_codes);
+
+            param.f_prepare = [reg_codes, val](cpu_ctx_t *cpu) {
+                uint32_t *p_reg_src;
+                cpu_decode_reg(cpu, (reg_codes >> 4) & 0x0F, &p_reg_src);
+                *p_reg_src = val;
+            };
+            param.expected_value = val;
+            param.f_get_actual_value = [reg_codes](cpu_ctx_t *cpu) {
+                uint32_t *p_reg_dst;
+                cpu_decode_reg(cpu, (reg_codes >> 0) & 0x0F, &p_reg_dst);
+                return *p_reg_dst;
+            };
+            v.push_back(param);
+        }
+        return v;
+    }()));
