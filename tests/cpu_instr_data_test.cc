21cbed7736dbd5f8e4c27487ce935d18c968bac5
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-05-01 10:42:23 +0300

    tests: move several helper functions into common.h

--- tests/cpu_instr_data_test.cc
+++ tests/cpu_instr_data_test.cc
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <random>
 
+#include "common.h"
 #include "cpu.h"
 #include "fake_mem.h"
 
@@ -86,50 +87,6 @@ TEST_P(DataInstrTest, WritesValue) {
     EXPECT_EQ(param.f_get_actual_value(param, cpu), param.expected_value);
 }
 
-static vm_addr_t get_random_base_addr(std::mt19937 &rng) {
-    std::uniform_int_distribution<vm_addr_t> addr_dist(0, VM_MAX_ADDR);
-    return addr_dist(rng);
-}
-
-static vm_addr_t get_random_data_addr(std::mt19937 &rng, vm_addr_t mem_base,
-                                      size_t min_bytes_left) {
-    vm_addr_t data_start = mem_base + TEST_MEM_START_DATA;
-    vm_addr_t data_end = mem_base + TEST_MEM_SIZE;
-    std::uniform_int_distribution<vm_addr_t> addr_dist(
-        data_start, data_end - min_bytes_left);
-    return addr_dist(rng);
-}
-
-static uint8_t get_random_reg_code(std::mt19937 &rng, bool unique_regs = false,
-                                   std::vector<uint8_t> used_reg_codes = {}) {
-    std::array<uint8_t, 9> reg_codes = {
-        CPU_CODE_R0, CPU_CODE_R1, CPU_CODE_R2, CPU_CODE_R3, CPU_CODE_R4,
-        CPU_CODE_R5, CPU_CODE_R6, CPU_CODE_R7, CPU_CODE_SP,
-    };
-    static_assert(CPU_NUM_GP_REG_CODES == 8, "update test code");
-    std::uniform_int_distribution<size_t> reg_dist(0, reg_codes.size() - 1);
-    uint8_t reg_code;
-    do {
-        reg_code = reg_codes[reg_dist(rng)];
-    } while (unique_regs &&
-             std::find(used_reg_codes.cbegin(), used_reg_codes.cend(),
-                       reg_code) != used_reg_codes.cend());
-    return reg_code;
-}
-
-static uint8_t get_random_reg_codes(std::mt19937 &rng, bool unique_regs = {},
-                                    std::vector<uint8_t> used_reg_codes = {}) {
-    uint8_t reg_code1 = get_random_reg_code(rng, unique_regs, used_reg_codes);
-    used_reg_codes.push_back(reg_code1);
-    uint8_t reg_code2 = get_random_reg_code(rng, unique_regs, used_reg_codes);
-    return (reg_code1 << 4) | reg_code2;
-}
-
-static uint32_t get_random_imm32(std::mt19937 &rng) {
-    std::uniform_int_distribution<uint32_t> val_dist(0, UINT32_MAX);
-    return val_dist(rng);
-}
-
 enum class ImmOperandRole {
     None,
     ExpVal,
@@ -168,7 +125,10 @@ static DataInstrParam get_random_param(
     uint32_t mem_base = get_random_base_addr(rng);
 
     std::optional<uint32_t> mem_addr;
-    if (gen_mem_addr) { mem_addr = get_random_data_addr(rng, mem_base, 4); }
+    if (gen_mem_addr) {
+        mem_addr = get_random_data_addr(rng, mem_base + TEST_MEM_START_DATA,
+                                        mem_base + TEST_MEM_SIZE, 4);
+    }
 
     std::optional<int32_t> mem_offset;
     if (gen_mem_addr && gen_mem_offset) {
@@ -180,7 +140,8 @@ static DataInstrParam get_random_param(
         // Truncated version is saved in mem_offset and can be used, since it's
         // what is encoded and together with mem_addr points to a valid RAM
         // dword.
-        uint32_t offseted_addr = get_random_data_addr(rng, mem_base, 4);
+        uint32_t offseted_addr = get_random_data_addr(
+            rng, mem_base + TEST_MEM_START_DATA, mem_base + TEST_MEM_SIZE, 4);
         int32_t offset = offseted_addr - *mem_addr;
         mem_offset = static_cast<int32_t>(offset);
     }
@@ -255,16 +216,6 @@ static DataInstrParam get_random_param(
         .f_prepare = f_prepare,
         .f_get_actual_value = f_get_act_val,
     };
-}
-
-uint32_t *get_reg_ptr(cpu_ctx_t *cpu, uint8_t reg_code) {
-    uint32_t *p_reg;
-    cpu_decode_reg(cpu, reg_code, &p_reg);
-    if (!p_reg) {
-        fprintf(stderr, "failed to decode register code 0x%02X\n", reg_code);
-        abort();
-    }
-    return p_reg;
 }
 
 INSTANTIATE_TEST_SUITE_P(
