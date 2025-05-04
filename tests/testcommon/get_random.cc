d4f6395447be242d93ac5fc651c511b68a32794a
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-05-04 14:50:11 +0300

    testcommon: add get_random_opcode()

--- tests/testcommon/get_random.cc
+++ tests/testcommon/get_random.cc
@@ -18,6 +18,31 @@ vm_addr_t get_random_data_addr(std::mt19937 &rng, vm_addr_t data_start,
     return addr_dist(rng);
 }
 
+uint8_t get_random_opcode(std::mt19937 &rng) {
+    constexpr std::array<uint8_t, 71> opcodes = {
+        CPU_OP_MOV_VR,   CPU_OP_MOV_RR,    CPU_OP_STR_RV0,  CPU_OP_STR_RI0,
+        CPU_OP_STR_RI8,  CPU_OP_STR_RI32,  CPU_OP_STR_RIR,  CPU_OP_LDR_RV0,
+        CPU_OP_LDR_RI0,  CPU_OP_LDR_RI8,   CPU_OP_LDR_RI32, CPU_OP_LDR_RIR,
+        CPU_OP_ADD_RR,   CPU_OP_ADD_RV,    CPU_OP_SUB_RR,   CPU_OP_SUB_RV,
+        CPU_OP_MUL_RR,   CPU_OP_MUL_RV,    CPU_OP_DIV_RR,   CPU_OP_DIV_RV,
+        CPU_OP_IDIV_RR,  CPU_OP_IDIV_RV,   CPU_OP_AND_RR,   CPU_OP_AND_RV,
+        CPU_OP_OR_RR,    CPU_OP_OR_RV,     CPU_OP_XOR_RR,   CPU_OP_XOR_RV,
+        CPU_OP_NOT_R,    CPU_OP_SHL_RR,    CPU_OP_SHL_RV,   CPU_OP_SHR_RR,
+        CPU_OP_SHR_RV,   CPU_OP_ROR_RR,    CPU_OP_ROR_RV,   CPU_OP_ROL_RR,
+        CPU_OP_ROL_RV,   CPU_OP_CMP_RR,    CPU_OP_TST_RR,   CPU_OP_TST_RV,
+        CPU_OP_JMPR_V8,  CPU_OP_JMPA_V32,  CPU_OP_JMPA_R,   CPU_OP_JEQR_V8,
+        CPU_OP_JEQA_V32, CPU_OP_JEQA_R,    CPU_OP_JNER_V8,  CPU_OP_JNEA_V32,
+        CPU_OP_JNEA_R,   CPU_OP_JGTR_V8,   CPU_OP_JGTA_V32, CPU_OP_JGTA_R,
+        CPU_OP_JGER_V8,  CPU_OP_JGEA_V32,  CPU_OP_JGEA_R,   CPU_OP_JLTR_V8,
+        CPU_OP_JLTA_V32, CPU_OP_JLTA_R,    CPU_OP_JLER_V8,  CPU_OP_JLEA_V32,
+        CPU_OP_JLEA_R,   CPU_OP_CALLA_V32, CPU_OP_CALLA_R,  CPU_OP_RET,
+        CPU_OP_PUSH_V32, CPU_OP_PUSH_R,    CPU_OP_POP_R,    CPU_OP_NOP,
+        CPU_OP_HALT,     CPU_OP_INT_V8,    CPU_OP_IRET,
+    };
+    std::uniform_int_distribution<uint8_t> opcode_dist(0, opcodes.size() - 1);
+    return opcode_dist(rng);
+}
+
 uint32_t *get_reg_ptr(cpu_ctx_t *cpu, uint8_t reg_code) {
     uint32_t *p_reg;
     cpu_decode_reg(cpu, reg_code, &p_reg);
--- tests/testcommon/get_random.h
+++ tests/testcommon/get_random.h
@@ -10,6 +10,8 @@ vm_addr_t get_random_base_addr(std::mt19937 &rng);
 vm_addr_t get_random_data_addr(std::mt19937 &rng, vm_addr_t data_start,
                                vm_addr_t data_end, size_t min_bytes_left);
 
+uint8_t get_random_opcode(std::mt19937 &rng);
+
 uint32_t *get_reg_ptr(cpu_ctx_t *cpu, uint8_t reg_code);
 uint8_t get_random_reg_code(std::mt19937 &rng, bool unique_regs = false,
                             std::vector<uint8_t> used_reg_codes = {});
