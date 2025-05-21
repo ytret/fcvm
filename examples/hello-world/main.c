0e5ae18545e139f70ef70945f42319e5185f4d25
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-05-21 22:14:00 +0300

    examples/hello-world: better file structure

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -19,63 +19,44 @@ static vm_ctx_t *g_vm = NULL;
 static file_rom_ctx_t *g_file_rom = NULL;
 static print_dev_ctx_t *g_print_dev = NULL;
 
+static int install_sigint_handler(void);
 static void sigint_handler(int signum);
+
+static int init(void);
+static int vm_loop(void);
 static void cleanup(void);
 
 int main(void) {
-    g_file_rom = file_rom_new(EX_HELLO_WORLD_GUEST);
-    if (!g_file_rom) {
-        fprintf(stderr, "main: file_rom_new() returned NULL\n");
+    int err_code = 0;
+    if ((err_code = install_sigint_handler()) != 0) {
         cleanup();
-        return 1;
+        return err_code;
+    }
+    if ((err_code = init()) != 0) {
+        cleanup();
+        return err_code;
     }
 
-    g_print_dev = print_dev_new();
-    if (!g_print_dev) {
-        fprintf(stderr, "main: print_dev_new() returned NULL\n");
+    fprintf(stderr, "%s: starting the VM, press Ctrl-C to exit\n", __func__);
+    if ((err_code = vm_loop()) != 0) {
         cleanup();
-        return 1;
+        return err_code;
     }
 
-    g_vm = vm_new();
-    vm_err_t err = vm_connect_dev(g_vm, &g_file_rom->desc, g_file_rom);
-    if (err != VM_ERR_NONE) {
-        fprintf(stderr, "main: could not connect file_rom, error %u\n", err);
-        cleanup();
-        return 1;
-    }
-    err = vm_connect_dev(g_vm, &g_print_dev->desc, g_print_dev);
-    if (err != VM_ERR_NONE) {
-        fprintf(stderr, "main: could not connect print_dev, error %u\n", err);
-        cleanup();
-        return 1;
-    }
+    fprintf(stderr, "%s: caught signal %u\n", __func__, g_caught_signum);
+    cleanup();
+    return 0;
+}
 
-    fprintf(stderr, "main: starting the VM, press Ctrl-C to exit\n");
-
+static int install_sigint_handler(void) {
     struct sigaction sa;
     sa.sa_handler = sigint_handler;
     sigemptyset(&sa.sa_mask);
     if (sigaction(SIGINT, &sa, NULL) == -1) {
-        fprintf(stderr, "main: failed to install a SIGINT handler: %s\n",
-                strerror(errno));
-        vm_free(g_vm);
-        file_rom_free(g_file_rom);
-        print_dev_free(g_print_dev);
+        fprintf(stderr, "%s: failed to install a SIGINT handler: %s\n",
+                __func__, strerror(errno));
         return 1;
     }
-
-    while (g_running) {
-        vm_step(g_vm);
-
-        if (g_vm->cpu->num_nested_exc != 0) {
-            fprintf(stderr, "main: unexpected exception, shutting down\n");
-            break;
-        }
-    }
-
-    fprintf(stderr, "main: caught signal %u\n", g_caught_signum);
-    cleanup();
     return 0;
 }
 
@@ -84,8 +65,51 @@ static void sigint_handler(int signum) {
     g_caught_signum = signum;
 }
 
-void cleanup(void) {
-    fprintf(stderr, "cleanup: freeing resources\n");
+static int init(void) {
+    g_file_rom = file_rom_new(EX_HELLO_WORLD_GUEST);
+    if (!g_file_rom) {
+        fprintf(stderr, "%s: file_rom_new() returned NULL\n", __func__);
+        return 1;
+    }
+
+    g_print_dev = print_dev_new();
+    if (!g_print_dev) {
+        fprintf(stderr, "%s: print_dev_new() returned NULL\n", __func__);
+        return 1;
+    }
+
+    g_vm = vm_new();
+    vm_err_t err = vm_connect_dev(g_vm, &g_file_rom->desc, g_file_rom);
+    if (err != VM_ERR_NONE) {
+        fprintf(stderr, "%s: could not connect file_rom, error %u\n", __func__,
+                err);
+        return 1;
+    }
+    err = vm_connect_dev(g_vm, &g_print_dev->desc, g_print_dev);
+    if (err != VM_ERR_NONE) {
+        fprintf(stderr, "%s: could not connect print_dev, error %u\n", __func__,
+                err);
+        return 1;
+    }
+
+    return 0;
+}
+
+static int vm_loop(void) {
+    while (g_running) {
+        vm_step(g_vm);
+
+        if (g_vm->cpu->num_nested_exc != 0) {
+            fprintf(stderr, "%s: unexpected CPU exception, shutting down\n",
+                    __func__);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+static void cleanup(void) {
+    fprintf(stderr, "%s: freeing resources\n", __func__);
     if (g_vm) { vm_free(g_vm); }
     if (g_file_rom) { file_rom_free(g_file_rom); }
     if (g_print_dev) { print_dev_free(g_print_dev); }
