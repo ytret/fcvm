ae6813e2866904b5a22b55d90ed0575b1a74fc5d
Yuri Tretyakov <ytretyakov18@gmail.com>
2025-05-23 08:26:03 +0300

    Add src/portability.h

---
+++ src/portability.h
@@ -0,0 +1,18 @@
+/**
+ * @file portability.h
+ * Support header for portability between systems and compilers.
+ */
+
+#pragma once
+
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_ctz)
+#define HAS_BUILTIN_CTZ 1
+#endif
+#elif defined(__GNUC__) || defined(__clang__)
+#define HAS_BUILTIN_CTZ 1
+#endif
+
+#if HAS_BUILTIN_CTZ
+#define stdc_first_trailing_one(x) __builtin_ctz(x)
+#endif
